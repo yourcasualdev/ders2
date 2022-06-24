ca69cab4a4e0619b40638b836c7487502f92017a
nightvision <babalibrahim34@gmail.com>
2022-06-24 23:10:19 +0300

    ders2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import Number from './NumberComponenti';
 
 function App() {
+  const [number, setNumber] = useState(0);
+
+  useEffect(() => {
+    console.log(number)
+
+  }, [number])
+
+  const sayıyıArttır = () => {
+    setNumber(number + 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Number num={number} />
+      <br />
+      <button onClick={() => { sayıyıArttır() }}>Sayıyı Arttır</button>
     </div>
   );
 }
---
+++ src/NumberComponenti.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NumberComponenti = ({ num }) => {
+    return (
+        <div>
+            <h1>Numaranız: {num}</h1>
+        </div>
+    )
+}
+
+export default NumberComponenti
